f9746ab1d4983f69afca8ba9cfea962036956ef5
tylerccaceres <tyler.caceres@gmail.com>
2019-07-16 21:45:58 -0400

    added new functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,150 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac'
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants'
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd'
+    },
+    content: {
+      text: 'Je pense , donc je suis'
+    },
+    created_at: 1461113959088
+  },
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac'
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants'
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd'
+    },
+    content: {
+      text: 'Je pense , donc je suis'
+    },
+    created_at: 1461113959088
+  },
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac'
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants'
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd'
+    },
+    content: {
+      text: 'Je pense , donc je suis'
+    },
+    created_at: 1461113959088
+  },
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac'
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants'
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd'
+    },
+    content: {
+      text: 'Je pense , donc je suis'
+    },
+    created_at: 1461113959088
+  },
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac'
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants'
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd'
+    },
+    content: {
+      text: 'Je pense , donc je suis'
+    },
+    created_at: 1461113959088
+  }
+];
 
+const renderTweets = function(tweets) {
+  let arr = [];
+  for (let tweet of tweets) {
+    arr.push(createTweetElement(tweet));
+  }
+  $('#tweet-container').append(arr);
+};
+
+const createTweetElement = function(tweet) {
+  let newArticle = `
+        <article class="tweet">
+					<header>
+						<div class="article-header">
+							<span class="tweet-pic"><img src="${tweet.user.avatars}" />${tweet.user.name}</span>
+							<span class="tweet-username">${tweet.user.handle}</span>
+						</div>
+					</header>
+					<p>${tweet.content.text}</p>
+					<footer>
+						<div class="article-footer">
+							<span class="days-ago">${
+  tweet.created_at
+}</span><span class="footer-actions"><div class="icons"><img src="/images/flag.png"><img src="/images/retweet.png"><img src="/images/heart.png"></div></span>
+						</div>
+					</footer>
+				</article>`;
+
+  return newArticle;
+};
+
+$(document).ready(function() {
+  console.log('this runs');
+  renderTweets(data);
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,16 @@
-s
+$(document).ready(function() {
+  console.log('hello');
+  $('#new-tweet-input').on('keyup', function() {
+    let $counter = $(this)
+      .siblings('div')
+      .children('.counter');
+
+    let charsLeft = 140 - $(this).val().length;
+    $counter.text(charsLeft);
+    if (charsLeft < 0) {
+      $counter.addClass('invalid-font');
+    } else {
+      $counter.removeClass('invalid-font');
+    }
+  });
+});
